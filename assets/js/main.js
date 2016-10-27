aabe2abb171a24d66d57ee533538a8a28bb52800
Federico Villedary <federico.villedary@gmail.com>
2016-10-27 17:24:03 -0300

    Agregar Función de Randomize

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,22 @@
 (function($){
 	'use strict';
 
+	
+	(function($) {
+	  $.fn.randomize = function(tree, childElem) {
+	    return this.each(function() {
+	      var $this = $(this);
+	      if (tree) $this = $(this).find(tree);
+	      var unsortedElems = $this.children(childElem);
+	      var elems = unsortedElems.clone();
+
+	      elems.sort(function() { return (Math.round(Math.random())-0.5); });  
+
+	      for(var i=0; i < elems.length; i++)
+		unsortedElems.eq(i).replaceWith(elems[i]);
+	    });    
+	  };
+	})(jQuery);
 /* --------------------------------------------------
 	Initialization
 -------------------------------------------------- */
@@ -8,6 +24,10 @@
     // Initialize all functions when the document is ready.
 	$(document).ready(function(){
 
+		/* Randomize Mmembers */
+		$("section#participantes").randomize("div.row", "div.member");
+		/* ------------------ */
+				
 		initNavbar();
 		initScroller();
 		initCountCirc();
