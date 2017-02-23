d86d0cd55af2c9b74a4feb6077d2dec676ea92dd
Federico Villedary <federico.villedary@gmail.com>
2017-02-23 02:18:33 -0300

    Save adhere form data in google sheets

--- assets/php/adhere-form.php
+++ assets/php/adhere-form.php
@@ -3,27 +3,41 @@ header('Content-Type: text/html; charset=utf-8');
 ob_start();
 
 include("config.php"); 
+include("googlesheets.php"); 
 
 //Seleccionamos el ultimo
 $match = "SELECT * FROM tbAdherentes order by adhID desc LIMIT 1;";
-$qry = mysql_query($match)
-or die ("No se pudieron encontrar datos porque ".mysql_error()); 
-$info = mysql_fetch_array( $qry );
+$qry = mysqli_query($link,$match)
+or die ("No se pudieron encontrar datos porque ".mysqli_error($link)); 
+$info = mysqli_fetch_array( $qry );
 
 $id = 0;
 if($info != null)
     $id = $info['adhID'] + 1;
 
+$data = array(
+    "nombre"  => utf8_decode(str_replace("'","&#39;",$_GET['name'])),
+    "email" => utf8_decode(str_replace("'","&#39;",$_GET['email'])),
+    "pais" => $_GET['pais'],
+    "tipo" => $_GET['tipo'],
+    "fecha" => date("Y-m-d H:i:s")
+);
+
+echo $id . " | " . implode("|",$data);
+
 //Agregamos el nuevo
 $match = "insert into tbAdherentes values (".
     ($id).", ".
-    $_GET['tipo'].", '".
-    utf8_decode(str_replace("'","&#39;",$_GET['name']))."', '".
-    utf8_decode(str_replace("'","&#39;",$_GET['email']))."', '".
-    $_GET['pais']."');";
+    $data['tipo'].", '".
+    $data['nombre']."', '".
+    $data['email']."', '".
+    $data['pais']."', '".
+    date("Y-m-d H:i:s")."');";
 
-$qry = mysql_query($match)
-or die ("No se pudo insertar porque ".mysql_error());
+$qry = mysqli_query($link,$match)
+or die ("No se pudo insertar porque ".mysqli_error($link));
+
+EscribirFilaGoogleSheets($data);
 
 echo 'ok';
 
